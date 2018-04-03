331b9b0c47ec1300a070a2fb69a103ce98295425
SteveM1993 <37126450+SteveM1993@users.noreply.github.com>
2018-04-03 02:38:12 -0500

    Update MainActivity.cs

--- source/MainActivity.cs
+++ source/MainActivity.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 using Android.App;
 using Android.Content;
@@ -9,26 +10,57 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
 
 namespace TODOQuestApp
 {
-    [Activity(Label = "MainActivity")]
+    [Activity(Label = "MainActivity", Icon = "@drawable/icon", MainLauncher = true)]
     public class MainActivity : Activity
     {
         Button addQuest;
+        //DB path
+        static string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "QuestsDb.db3");
+        //DB variable
+        SQLiteConnection db = new SQLiteConnection(path);
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            
+            //Call method to create tables if it doesn't exist
+            DBCheckAndCreate();
 
             //Open Main Menu
             SetContentView(Resource.Layout.Main);
+
+            //Call method to load the quest list
+            LoadQuestList();
 
             addQuest = FindViewById<Button>(Resource.Id.addButton);
             addQuest.Click += delegate
             {
                 StartActivity(typeof(AddQuest));
             };
+        }
+
+        //Method to create db if it doesn't exist
+        private void DBCheckAndCreate()
+        {
+            var info = db.GetTableInfo("Quests");
+            if (!info.Any())
+            {
+                db.CreateTable<Quests>();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        //Method to load quest list
+        public void LoadQuestList()
+        {
+            
         }
     }
 }
