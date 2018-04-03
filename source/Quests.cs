780c435ed67235a4181dcd3f9035b0471b0e1c71
SteveM1993 <37126450+SteveM1993@users.noreply.github.com>
2018-04-02 23:26:38 -0500

    Add files via upload

---
+++ source/Quests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TODOQuestApp
+{
+    [Serializable]
+    class Quests
+    {
+        public string questName;
+        public string dueDate;
+        public string difficulty;
+
+        public Quests(string name, string due, string diff)
+        {
+            this.questName = name;
+            this.dueDate = due;
+            this.difficulty = diff;
+        }
+    }
+}
