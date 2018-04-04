61a239b6bc407dabdc63450945bd681feb5d2cd6
SteveM1993 <37126450+SteveM1993@users.noreply.github.com>
2018-04-04 04:16:08 -0500

    Update MainActivity.cs

--- source/MainActivity.cs
+++ source/MainActivity.cs
@@ -14,7 +14,7 @@ using SQLite;
 
 namespace TODOQuestApp
 {
-    [Activity(Label = "MainActivity", Icon = "@drawable/icon", MainLauncher = true)]
+    [Activity(Label = "MainActivity", MainLauncher = true)]
     public class MainActivity : Activity
     {
         Button addQuest;
@@ -22,6 +22,7 @@ namespace TODOQuestApp
         static string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "QuestsDb.db3");
         //DB variable
         SQLiteConnection db = new SQLiteConnection(path);
+        Quest[] questList;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -49,7 +50,7 @@ namespace TODOQuestApp
             var info = db.GetTableInfo("Quests");
             if (!info.Any())
             {
-                db.CreateTable<Quests>();
+                db.CreateTable<Quest>();
             }
             else
             {
@@ -60,7 +61,60 @@ namespace TODOQuestApp
         //Method to load quest list
         public void LoadQuestList()
         {
-            
+            //Array to hold quests
+            questList = new Quest[30];
+
+            //Populate array from db
+            var table = db.Table<Quest>();
+            int count = 0;
+            foreach (var i in table)
+            {
+                questList[count] = i;
+                count++;
+            }
+
+            //Create list view for quests
+            ListView questsView = FindViewById<ListView>(Resource.Id.quests);
+            questsView.Adapter = new QuestListAdapter(this, questList);
+
+            //Register list view for context menu
+            RegisterForContextMenu(questsView);
+        }
+
+        //Method for creating context menu
+        public override void OnCreateContextMenu(IContextMenu menu, View v, IContextMenuContextMenuInfo menuInfo)
+        {
+            if(v.Id == Resource.Id.quests)
+            {
+                var info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+                menu.SetHeaderTitle("Quest Menu");
+                var menuItems = Resources.GetStringArray(Resource.Array.menu);
+                for(int i = 0; i < menuItems.Length; i++)
+                {
+                    menu.Add(Menu.None, i, i, menuItems[i]);
+                }
+            }
+        }
+
+        //Method for handling when menu item is selected
+        public override bool OnContextItemSelected(IMenuItem item)
+        {
+            var info = (AdapterView.AdapterContextMenuInfo)item.MenuInfo;
+            var index = item.ItemId;
+            var menuItem = Resources.GetStringArray(Resource.Array.menu);
+            var menuItemName = menuItem[index];
+            var questName = questList[info.Position];
+
+            //Check if complete was selected
+            if (menuItemName == "Complete")
+            {
+
+
+                //Delete quest from db
+                db.Delete(questName);
+            }
+
+            return true;
         }
     }
 }
